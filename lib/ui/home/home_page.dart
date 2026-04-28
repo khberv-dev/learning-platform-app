8925393a2ebc2621d0cda487d2fd8c9eb6b035ce
Asror <khberv.dev@gmail.com>
2026-04-28 15:45:46 +0500

    refactor

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:student/app/theme/app_spacing.dart';
-import 'package:student/ui/home/widget/ai_assessment_card.dart';
-import 'package:student/ui/home/widget/home_header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    void onNotificationClick() {}
-
     return SingleChildScrollView(
       padding: EdgeInsets.all(AppSpacing.md),
-      child: Column(
-        children: [
-          HomeHeader(onNotificationClick: onNotificationClick),
-          SizedBox(height: AppSpacing.md),
-          AiAssessmentCard(),
-        ],
-      ),
+      child: Column(children: []),
     );
   }
 }
